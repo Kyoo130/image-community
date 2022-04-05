b106c8058dac41e0f50f33e220f342fdae4f3b08
Kyoo130 <kml9003@kakao.com>
2022-04-05 22:07:05 +0900

    :sparkles: firebase 연동 및 회원가입 기능 개발

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,31 +1,75 @@
-import React from 'react';
-import {Button, Grid, Input, Text} from "../elements";
+import React, { useState } from "react";
+import { Button, Grid, Input, Text } from "../elements";
+
+import { useDispatch } from "react-redux";
+import { actionCreators as userActions } from "../redux/modules/user";
 
 const Signup = () => {
+  const dispatch = useDispatch();
+
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [pwdCheck, setPwdCheck] = useState("");
+  const [user_Name, setUserName] = useState("");
+
+  const signup = () => {
+    if (id === "" || pwd === "" || user_Name === "") {
+      return;
+    }
+    if (pwd !== pwdCheck) {
+      return;
+    }
+    dispatch(userActions.signupFB(id, pwd, user_Name));
+  };
+
   return (
     <>
       <Grid padding="16px">
-        <Text size="32px" bold>회원가입</Text>
+        <Text size="32px" bold>
+          회원가입
+        </Text>
         <Grid padding="16px 0">
-          <Input label="아이디" placeholder="아이디를 입력해주세요." _onChange={() => {console.log("아이디 입력 완료!")}} />
+          <Input
+            label="아이디"
+            placeholder="아이디를 입력해주세요."
+            _onChange={(e) => {
+              setId(e.target.value);
+            }}
+          />
         </Grid>
         <Grid padding="16px 0">
-          <Input label="닉네임" placeholder="닉네임을 입력해주세요." _onChange={() => {console.log("닉네임 입력 완료!")}} />
+          <Input
+            label="닉네임"
+            placeholder="닉네임을 입력해주세요."
+            _onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+          />
         </Grid>
         <Grid padding="16px 0">
-          <Input label="비밀번호" placeholder="비밀번호를 입력해주세요." _onChange={() => {console.log("비밀번호 입력 완료!")}} />
+          <Input
+            label="비밀번호"
+            placeholder="비밀번호를 입력해주세요."
+            _onChange={(e) => {
+              setPwd(e.target.value);
+            }}
+          />
         </Grid>
         <Grid padding="16px 0">
-          <Input label="비밀번호 확인" placeholder="비밀번호를 다시 입력해주세요." _onChange={() => {console.log("비밀번호 확인 입력 완료!")}} />
+          <Input
+            label="비밀번호 확인"
+            placeholder="비밀번호를 다시 입력해주세요."
+            _onChange={(e) => {
+              setPwdCheck(e.target.value);
+            }}
+          />
         </Grid>
-        <Button text="회원가입하기" />
+        <Button text="회원가입하기" _onClick={signup} />
       </Grid>
     </>
   );
 };
 
-Signup.defaultProps = {
-
-}
+Signup.defaultProps = {};
 
 export default Signup;
