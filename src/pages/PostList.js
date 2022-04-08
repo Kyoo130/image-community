ad3b0f32047e3e57565c746105e2d641282e7e96
Kyoo130 <kml9003@kakao.com>
2022-04-09 00:54:16 +0900

    :sparkles: 무한 스크롤 기능 및 Spinner 컴포넌트 개발

--- src/pages/PostList.js
+++ src/pages/PostList.js
@@ -3,11 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { Post } from "../components";
 import { actionCreators as postActions } from "../redux/modules/post";
+import InfinityScroll from "../shared/InfinityScroll";
 
 const PostList = () => {
   const dispatch = useDispatch();
   const post_list = useSelector((state) => state.post.list);
   const user_info = useSelector((state) => state.user.user);
+  const is_loading = useSelector((state) => state.post.is_loading);
+  const paging = useSelector((state) => state.post.paging);
 
   useEffect(() => {
     if (post_list.length === 0) {
@@ -17,13 +20,21 @@ const PostList = () => {
 
   return (
     <>
-      {post_list.map((it, idx) => {
+      <InfinityScroll
+        callNext={() => {
+          dispatch(postActions.getPostFB(paging.next));
+        }}
+        is_next={!!paging.next}
+        loading={is_loading}
+      >
+      {post_list.map((it) => {
         if(it.user_info.user_id === user_info?.uid) {
           return <Post key={it.id} {...it} is_me />;
         } else {
           return <Post key={it.id} {...it} />
         }
       })}
+      </InfinityScroll>
     </>
   );
 };
--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -9,18 +9,22 @@ import { actionCreators as imageActions } from "./image";
 const SET_POST = "SET_POST";
 const ADD_POST = "ADD_POST";
 const EDIT_POST = "EDIT_POST";
+const LOADING = "LOADING";
 
 // action creators
-const setPost = createAction(SET_POST, (post_list) => ({ post_list }));
+const setPost = createAction(SET_POST, (post_list, paging) => ({ post_list, paging }));
 const addPost = createAction(ADD_POST, (post) => ({ post }));
 const editPost = createAction(EDIT_POST, (post_id, post) => ({
   post_id,
   post,
 }));
+const loading = createAction(LOADING, (is_loading) => ({ is_loading }));
 
 // initialState
 const initialState = {
   list: [],
+  paging: { start: null, next: null, size: 3 },
+  is_loading: false,
 };
 
 const initialPost = {
@@ -50,12 +54,10 @@ const editPostFB = (post_id = null, post = {}) => {
       postDB
         .doc(post_id)
         .update(post)
-        .then((doc) => {
+        .then(() => {
           dispatch(editPost(post_id, { ...post }));
           history.replace("/");
         });
-
-      return;
     } else {
       const user_id = getState().user.user.uid;
       const _upload = storage
@@ -72,7 +74,7 @@ const editPostFB = (post_id = null, post = {}) => {
             postDB
               .doc(post_id)
               .update({ ...post, image_url: url })
-              .then((doc) => {
+              .then(() => {
                 dispatch(editPost(post_id, { image_url: url }));
                 history.replace("/");
               });
@@ -138,31 +140,60 @@ const addPostFB = (contents = "") => {
   };
 };
 
-const getPostFB = () => {
+const getPostFB = (start = null, size = 3) => {
   return function (dispatch, getState, { history }) {
+
+    let _paging = getState().post.paging;
+
+    if(_paging.start && !_paging.next){
+      console.log("불러올 데이터가 없습니다.")
+      return;
+    }
+
+    dispatch(loading(true));
     const postDB = firestore.collection("post");
 
-    postDB.get().then((docs) => {
-      let post_list = [];
-      docs.forEach((doc) => {
-        let _post = doc.data();
+    let query = postDB.orderBy("insert_dt", "desc");
 
-        let post = Object.keys(_post).reduce(
-          (acc, cur) => {
-            if (cur.indexOf("user_") !== -1) {
-              return {
-                ...acc,
-                user_info: { ...acc.user_info, [cur]: _post[cur] },
-              };
-            }
-            return { ...acc, [cur]: _post[cur] };
-          },
-          { id: doc.id, user_info: {} }
-        );
-        post_list.push(post);
+    if (start) {
+      query = query.startAt(start);
+    }
+
+    query
+      .limit(size + 1)
+      .get()
+      .then((docs) => {
+        let post_list = [];
+
+        let paging = {
+          start: docs.docs[0],
+          next:
+            docs.docs.length === size + 1
+              ? docs.docs[docs.docs.length - 1]
+              : null,
+          size: size,
+        };
+
+        docs.forEach((doc) => {
+          let _post = doc.data();
+
+          let post = Object.keys(_post).reduce(
+            (acc, cur) => {
+              if (cur.indexOf("user_") !== -1) {
+                return {
+                  ...acc,
+                  user_info: { ...acc.user_info, [cur]: _post[cur] },
+                };
+              }
+              return { ...acc, [cur]: _post[cur] };
+            },
+            { id: doc.id, user_info: {} }
+          );
+          post_list.push(post);
+        });
+        post_list.pop();
+        dispatch(setPost(post_list, paging));
       });
-      dispatch(setPost(post_list));
-    });
   };
 };
 
@@ -171,7 +202,9 @@ export default handleActions(
   {
     [SET_POST]: (state, action) =>
       produce(state, (draft) => {
-        draft.list = action.payload.post_list;
+        draft.list.push(...action.payload.post_list);
+        draft.paging = action.payload.paging;
+        draft.is_loading = false;
       }),
     [ADD_POST]: (state, action) =>
       produce(state, (draft) => {
@@ -181,6 +214,10 @@ export default handleActions(
       produce(state, (draft) => {
         let idx = draft.list.findIndex((p) => p.id === action.payload.post_id);
         draft.list[idx] = { ...draft.list[idx], ...action.payload.post };
+      }),
+    [LOADING]: (state, action) =>
+      produce(state, (draft) => {
+        draft.is_loading = action.payload.is_loading;
       }),
   },
   initialState
